35af3211d566d3aa5c70404ccb3fba2108357d02
Sara Bartell <saracbartell@gmail.com>
2018-01-04 16:13:50 -0800

    Finish API call

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,4 +1,16 @@
 class TweetController < ApplicationController
+  include TweetHelper
+
   def index
+    tweets = $client.user_timeline('rubyinside', count: 3)
+    p tweets
+    p "*****"
+    tweets.each do |tweet|
+      p tweet
+       tweet.full_text
+
+    end
+
   end
+
 end
